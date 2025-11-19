261de00077f499591a23d220767ff12d0897289e
mvxindra <50985483+mvxindra@users.noreply.github.com>
2025-11-19 17:14:11 -0500

    Delete Assets/DataModels directory

--- Assets/DataModels/DropEntry.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-[Serializable]
-public class DropEntry
-{
-    public string reward;  // "GOLD:2000", "GEAR_SWORD_A1", etc.
-    public int weight;
-}
--- Assets/DataModels/Wave.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-[Serializable]
-public class Wave
-{
-    public List<string> enemyParty;  // List of enemy IDs for this wave
-}
